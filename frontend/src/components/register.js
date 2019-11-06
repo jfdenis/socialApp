c3438b70536f03986da4623eff967f4c73097dc2
CinderyCape27 <julio_denis@icloud.com>
2019-11-06 17:23:06 -0600

    componente registro completado

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -3,15 +3,33 @@ import axios from 'axios';
 export default class register extends Component {
 
     state = {
+        name: '',
+        last_name: '',
+        email: '',
+        password: '',
         users: []
     }
-
+    
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/users')
+        console.log(res);
+    }
     onSubmit = async (e) => {
-        e.preventDefault();
-        const newUser = await axios.post('http://localhost:4000/api/users', {
-            users: this.state
+        const newUser = {
+            name: this.state.name,
+            last_name: this.state.last_name,
+            email: this.state.email,
+            password: this.state.password
+        }
+        
+
+        await axios.post('http://localhost:4000/signup', newUser)
+    }
+    onInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
         })
-        console.log(newUser)
+
     }
 
     render() {
@@ -24,30 +42,54 @@ export default class register extends Component {
                                 <div className="card-body">
                                     <h3 className="text-center">Social App</h3>
                                 </div>
-                                <h4 className="text-center" >Creat your SocialApp account  </h4>
+                                <h4 className="text-center" >Create your SocialApp account  </h4>
                                 <hr />
                                 <form onSubmit={this.onSubmit}>
                                     <div className="form-row">
                                         <div className="form-group col-md-6">
-                                            <input type="text" placeholder="Name" className="form-control"/>
+                                            <input 
+                                                type="text" 
+                                                placeholder="Name" 
+                                                className="form-control" 
+                                                onChange={this.onInputChange}
+                                                name="name" 
+                                                />
                                         </div>
                                         <div className="form-group col-md-6">
-                                            <input type="text" className="form-control" placeholder="Last name"/>
+                                            <input 
+                                            type="text"  
+                                            className="form-control" 
+                                            placeholder="Last name" 
+                                            onChange={this.onInputChange}
+                                            name="last_name"
+                                            />
+                                            
                                         </div>
-                                        
                                         <div className="form-group ml-2">
-                                            <input type="text" className="form-control" placeholder="Email" required />
+                                            <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            placeholder="Email" 
+                                            onChange={this.onInputChange}
+                                            name="email" 
+                                            required />
                                         </div>
                                         <div className="form-group ml-2">
-                                            <input type="password" className="form-control" placeholder="Password" required />
+                                            <input 
+                                            type="password" 
+                                            className="form-control" 
+                                            placeholder="Password" 
+                                            onChange={this.onInputChange}
+                                            name="password" 
+                                            required />
                                             <small className="text-sm-left">Use 8 or more characters with a mix of letters, numbers & symbols</small>
                                         </div>
                                     </div>
+                                    <button type="submit" className="btn btn-primary">
+                                        Register
+                                    </button>
 
                                 </form>
-                                <button type="submit" className="btn btn-primary">
-                                    Register
-                                </button>
                             </div>
 
                         </div>
